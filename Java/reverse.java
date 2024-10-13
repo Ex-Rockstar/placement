a47b5873629ac252e5369b65436b92b04ef60c1b
Manasa <manasa.sanriya@gmail.com>
2024-10-13 20:02:21 +0530

    Reverse

---
+++ Java/reverse.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public long reverse_digit(long n)
+    {
+        
+        long reverse = 0;
+        while(n!=0){
+            long remainder = n%10;
+            reverse = reverse * 10 + remainder;
+            n=n/10;
+        }
+        return reverse;
+    }
+}
