aa99870090a7de7124e404171574ef013de14310
Ruthi Shankari <ruthirockstar2218@gmail.com>
2024-11-06 02:14:09 -0800

    Update factorialRecursion.cpp

--- c++/other problems i solved/factorialRecursion.cpp
+++ c++/other problems i solved/factorialRecursion.cpp
@@ -1,12 +1,6 @@
-    int fib(int n) {
-        if (n == 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
-
-    // Recursive case: F(n) = F(n-1) + F(n-2)
-    return fib(n - 1) + fib(n - 2);
-        
+long long int factorial(int N){
+        if(N==1){
+            return 1;
+        }
+        return N*factorial(N-1);
     }
